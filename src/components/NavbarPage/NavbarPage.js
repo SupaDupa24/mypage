8d579ac1a5875a351deeba4cf4fdd86ad7aebb6d
SupaDupa24 <62140687+SupaDupa24@users.noreply.github.com>
2021-01-30 17:38:24 -0600

    nav

--- src/components/NavbarPage/NavbarPage.js
+++ src/components/NavbarPage/NavbarPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {
-MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse,
 MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
 } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -17,7 +17,7 @@ toggleCollapse = () => {
 render() {
   return (
     <Router>
-      <MDBNavbar color="black" dark expand="md" style={{position:'fixed'}}>
+      <MDBNavbar className='barbus' color="black" dark expand="md" >
         <MDBNavbarBrand>
           <strong className="white-text">I|G|X</strong>
         </MDBNavbarBrand>
@@ -40,31 +40,11 @@ render() {
               <MDBNavLink to="/projects">PROJECTS</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
-              <MDBNavLink to="#!">BLOGSPOT</MDBNavLink>
+              <MDBNavLink to="/blogspot">BLOGSPOT</MDBNavLink>
             </MDBNavItem>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret>
-                  <span className="mr-2">Dropdown</span>
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
+           
           </MDBNavbarNav>
-          <MDBNavbarNav right>
-            <MDBNavItem>
-              <MDBFormInline waves>
-                <div className="md-form my-0">
-                  <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                </div>
-              </MDBFormInline>
-            </MDBNavItem>
-          </MDBNavbarNav>
+
         </MDBCollapse>
       </MDBNavbar>
     </Router>
